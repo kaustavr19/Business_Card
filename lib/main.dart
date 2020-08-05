0229d274a3166cf7a0d339486864eeaeb905c7dc
kaustavr19 <kaustavr19@users.noreply.github.com>
2020-08-05 09:13:14 +0530

    A simple business card project

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,88 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.red.shade900,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/yo.jpg'),
+              ),
+              Text(
+                'Kaustav Roy',
+                style: TextStyle(
+                  fontFamily: 'SpecialElite',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                'Flutter Developer',
+                style: TextStyle(
+                  fontFamily: 'TitilliumWbe',
+                  fontSize: 20.0,
+                  color: Colors.red.shade100,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade50,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.blue,
+                  ),
+                  title: Text(
+                    '+91 9038540603',
+                    style: TextStyle(
+                      color: Colors.blue.shade500,
+                      fontFamily: 'TitilliumWeb',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.blue,
+                  ),
+                  title: Text(
+                    'kaustav574@gmail.com',
+                    style: TextStyle(
+                      color: Colors.blue.shade500,
+                      fontFamily: 'TitilliumWeb',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
